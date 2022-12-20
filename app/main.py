67a406d7e45fca6d1784761763dd1bdfaedfbf92
HM <hieumdd@gmail.com>
2022-12-20 17:43:22 +0700

    feat: Add fastapi app

---
+++ app/main.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+
+app = FastAPI()
+
+
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    return Jinja2Templates(
+        directory=f"{Path(__file__).resolve().parent}/templates"
+    ).TemplateResponse("index.html", {"request": request})
