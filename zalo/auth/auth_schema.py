19e88370cf4f8161fad8784f1ddfe2e385cc41fa
HM <hieumdd@gmail.com>
2022-12-29 10:13:14 +0700

    chore: Remove database & zalo module

--- zalo/auth/auth_schema.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-
-class AccessToken(BaseModel):
-    access_token: str
-    refresh_token: str
-    expires_in: str
--- zalo/auth/auth_service_test.py
+++
@@ -1,6 +0,0 @@
-from .auth_service import create_authorization_url
-
-
-def test_create_authorization_url():
-    url = create_authorization_url()
-    assert url
