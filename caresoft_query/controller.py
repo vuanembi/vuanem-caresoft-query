7e6d51db93a97d53608c28f57b6b811bab951861
HM <hieumdd@gmail.com>
2023-01-03 11:23:24 +0700

    feat: Add authlib client

--- caresoft_query/controller.py
+++ caresoft_query/controller.py
@@ -1,14 +1,11 @@
 from fastapi import APIRouter
 
 from caresoft_query.dto import CustomerResponse
-from caresoft_query.service import get_user_by_phone
+from caresoft_query.service import get_customer_by_phone
 
 controller = APIRouter()
 
 
-@controller.get(
-    "/query/customer/",
-    response_model= list[CustomerResponse],
-)
+@controller.get("/query/customer/", response_model=list[CustomerResponse])
 def get_customer(phone: str):
-    return get_user_by_phone(phone)
+    return get_customer_by_phone(phone)
--- netsuite/service.py
+++ netsuite/service.py
@@ -1,30 +1,50 @@
-import json
-import os
-from oauthlib.oauth1 import SIGNATURE_HMAC_SHA256
-from requests_oauthlib import OAuth1Session
+import time
+
+from authlib.integrations.httpx_client import OAuth1Client
+from fastapi import HTTPException
+
+from settings import settings
+from netsuite.interface import SuiteQLResponse
 
 
-def netsuite_session() -> OAuth1Session:
-    return OAuth1Session(
-        client_key=os.getenv("CONSUMER_KEY"),
-        client_secret=os.getenv("CONSUMER_SECRET"),
-        resource_owner_key=os.getenv("ACCESS_TOKEN"),
-        resource_owner_secret=os.getenv("TOKEN_SECRET"),
-        realm=os.getenv("ACCOUNT_ID"),
-        signature_method=SIGNATURE_HMAC_SHA256,
+def create_client() -> OAuth1Client:
+    return OAuth1Client(
+        client_id=settings.NS_CONSUMER_KEY,
+        client_secret=settings.NS_CONSUMER_SECRET,
+        token=settings.NS_ACCESS_TOKEN,
+        token_secret=settings.NS_TOKEN_SECRET,
+        realm=settings.NS_ACCOUNT_ID,
+        force_include_body=True,
+        base_url=f"https://{settings.NS_ACCOUNT_ID}.suitetalk.api.netsuite.com/services/rest",
     )
 
 
-def run_suiteql_query(session: OAuth1Session, sql: str) -> list[dict]:
-    account_id = os.getenv("ACCOUNT_ID")
-    headers = {"Prefer": "transient", "Content-Type": "application/json"}
-    with session.request(
-        method="POST",
-        url=f"https://{account_id}.suitetalk.api.netsuite.com/services/rest/query/v1/suiteql",
-        params={"limit": 1000, "offset": 0},
-        json={"q": sql},
-        headers=headers,
-    ) as r:
-        response_json = json.loads(r.text)
-        items = response_json["items"]
-        return items
+def query_suiteql(sql: str) -> list[dict]:
+    limit = 1000
+
+    def _request(client: OAuth1Client, offset: int = 0):
+        response = client.request(
+            method="POST",
+            url="query/v1/suiteql",
+            headers={"Prefer": "transient", "Content-Type": "application/json"},
+            params={"limit": limit, "offset": offset},
+            json={"q": sql},
+        )
+
+        if response.status_code == 200:
+            data: SuiteQLResponse = response.json()
+
+            return (
+                data["items"] + _request(client, offset + limit)
+                if data["hasMore"]
+                else data["items"]
+            )
+        elif response.status_code == 429:
+            time.sleep(5)
+            return _request(client, offset)
+        else:
+            print(response)
+            raise HTTPException(status_code=500, detail="NetSuite Error")
+
+    with create_client() as client:
+        return _request(client)
