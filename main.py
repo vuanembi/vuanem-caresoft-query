3ad00aa4ba7303890820096f579f402879f3f778
HM <hieumdd@gmail.com>
2023-01-03 14:16:48 +0700

    feat: Add app title

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 
 from caresoft_query.controller import controller as caresoft_query_controller
 
-app = FastAPI()
+app = FastAPI(
+    title="vuanem-caresoft-query",
+)
 
 app.include_router(caresoft_query_controller)
