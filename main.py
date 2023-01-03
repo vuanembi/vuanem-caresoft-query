68ca2760d7b753554422af089ba52770e788abaa
HM <hieumdd@gmail.com>
2023-01-03 15:16:24 +0700

    feat: Add CORS middleware

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from caresoft_query.controller import controller as caresoft_query_controller
 
 app = FastAPI(
     title="vuanem-caresoft-query",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(caresoft_query_controller)
